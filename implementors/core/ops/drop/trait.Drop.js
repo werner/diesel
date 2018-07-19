cbe36e690f72d4d158c96489ada29c5f3266d53c
Travis CI User <travis@example.org>
2018-07-19 18:09:07 +0000

    Documentation for diesel-rs/diesel@68d0b9e

---
+++ implementors/core/ops/drop/trait.Drop.js
@@ -0,0 +1,12 @@
+(function() {var implementors = {};
+implementors["r2d2"] = [{text:"impl&lt;M&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"r2d2/struct.PooledConnection.html\" title=\"struct r2d2::PooledConnection\">PooledConnection</a>&lt;M&gt; <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;M: <a class=\"trait\" href=\"r2d2/trait.ManageConnection.html\" title=\"trait r2d2::ManageConnection\">ManageConnection</a>,&nbsp;</span>",synthetic:false,types:["r2d2::PooledConnection"]},];
+implementors["scheduled_thread_pool"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"scheduled_thread_pool/struct.ScheduledThreadPool.html\" title=\"struct scheduled_thread_pool::ScheduledThreadPool\">ScheduledThreadPool</a>",synthetic:false,types:["scheduled_thread_pool::ScheduledThreadPool"]},];
+implementors["url"] = [{text:"impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"url/struct.PathSegmentsMut.html\" title=\"struct url::PathSegmentsMut\">PathSegmentsMut</a>&lt;'a&gt;",synthetic:false,types:["url::path_segments::PathSegmentsMut"]},{text:"impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"url/struct.UrlQuery.html\" title=\"struct url::UrlQuery\">UrlQuery</a>&lt;'a&gt;",synthetic:false,types:["url::UrlQuery"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
